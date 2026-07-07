50f36ee0a6b8e677414f75ec640978d922f5c786
lumesh2911-2 <lumesh@beeonline.co.in>
2026-07-07 16:24:24 +0530

    React navigation added.

---
+++ src/navigation/RootNavigationScreen.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from '../screens/SplashScreen';
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigationScreen() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
